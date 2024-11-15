ea3cce4da09e4eed6c597090b1809a97cf42c4c9
yizhou <tongyz2922@mails.jlu.edu.cn>
2024-11-15 16:50:12 +0800

    initial commit

---
+++ lua/modules/plugins/lang.lua
@@ -0,0 +1,42 @@
+local lang = {}
+
+lang["kevinhwang91/nvim-bqf"] = {
+	lazy = true,
+	ft = "qf",
+	config = require("lang.bqf"),
+	dependencies = {
+		{ "junegunn/fzf", build = ":call fzf#install()" },
+	},
+}
+lang["mrcjkb/rustaceanvim"] = {
+	lazy = true,
+	ft = "rust",
+	version = "*",
+	init = require("lang.rust"),
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
+lang["Saecki/crates.nvim"] = {
+	lazy = true,
+	event = "BufReadPost Cargo.toml",
+	config = require("lang.crates"),
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
+lang["MeanderingProgrammer/render-markdown.nvim"] = {
+	lazy = true,
+	ft = "markdown",
+	config = require("lang.render-markdown"),
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
+lang["iamcco/markdown-preview.nvim"] = {
+	lazy = true,
+	ft = "markdown",
+	build = ":call mkdp#util#install()",
+}
+lang["chrisbra/csv.vim"] = {
+	lazy = true,
+	ft = "csv",
+}
+return lang
---
+++ lua/user/settings.lua
@@ -0,0 +1,8 @@
+-- Please check `lua/core/settings.lua` to view the full list of configurable settings
+local settings = {}
+
+settings["use_ssh"] = true
+
+settings["colorscheme"] = "catppuccin"
+
+return settings
