9bed739cf25d1fd4ea9a15cb4c5a17b51184b99e
Don Yihtseu <i@yizhou.ac.cn>
2025-06-30 02:49:40 +0800

    feat(build): add build system integration with overseer

---
+++ lua/framework/build.lua
@@ -0,0 +1,51 @@
+local path = vim.fs.joinpath(".nvim", "build.json")
+local tasks = {}
+if vim.fn.filereadable(path) then
+    local file = io.open(path, "r")
+    if file then
+        local content = file:read("a")
+        file:close()
+        local success, json = pcall(vim.json.decode, content)
+        if success then
+            tasks = json.tasks
+        end
+    end
+end
+
+return {
+    "stevearc/overseer.nvim",
+    lazy = true,
+    keys = {
+        { "<leader>b", "<CMD>OverseerRun<CR>", "Run Tasks" }
+    },
+    opts = {
+        strategy = "toggleterm",
+        auto_detect_success_color = true,
+        task_list = {
+            direction = "right",
+        },
+        actions = {},
+        form = {
+            border = "rounded",
+        }
+    },
+    config = function(_, opts)
+        local overseer = require("overseer")
+        overseer.setup(opts)
+
+        for _, task in ipairs(tasks) do
+            overseer.register_template {
+                name = task.name,
+                builder = function()
+                    return {
+                        cmd = task.cmd,
+                        env = task.env or {},
+                        cwd = task.cwd or vim.fn.getcwd(),
+                        components = task.components or { "default" },
+                        output_file = task.output_file,
+                    }
+                end
+            }
+        end
+    end
+}
