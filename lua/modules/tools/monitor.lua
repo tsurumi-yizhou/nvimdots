5b6448c4cf22dac728f1e1240efea88e242dc0a4
Don Yihtseu <i@yizhou.ac.cn>
2025-08-10 01:51:02 +0800

    feat(monitor): 添加 fidget.nvim 插件配置
    添加 fidget.nvim 插件到监控模块，配置通知窗口的透明度为0

--- lua/modules/tools/monitor.lua
+++ lua/modules/tools/monitor.lua
@@ -4,4 +4,16 @@ return {
         lazy = true,
         event = "VeryLazy",
     },
+    {
+        "j-hui/fidget.nvim",
+        lazy = true,
+        event = "LspAttach",
+        opts = {
+            notification = {
+                window = {
+                    winblend = 0,
+                },
+            },
+        },
+    }
 }
